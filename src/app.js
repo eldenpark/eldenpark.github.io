8040f25057c1ddc219b3561e699160bf86919931
Elden S. Park <enginehenryed@gmail.com>
2018-01-29 08:52:39 +0900

    Remove unused files
    PR-URL: https://github.com/eldeni/eldeni.github.io/pull/18

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const minify = require('html-minifier').minify;
 const path = require('path');
 
 const compile = require('./utils/fileUtils').compile;
-const sassc = require('./sassc');
+const sassc = require('./utils/sassUtils').sassc;
 
 sassc();
 compile('index');
