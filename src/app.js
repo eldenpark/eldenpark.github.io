6841167e13b7745ff4fa007f828795c469d0a87d
eldeni <eldeniyenden@gmail.com>
2018-02-26 17:32:42 +0900

    chore: remove unused files

--- src/app.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const minify = require('html-minifier').minify;
-const path = require('path');
-
-const compile = require('./utils/fileUtils').compile;
-const sassc = require('./utils/sassUtils').sassc;
-
-sassc();
-compile('index');
